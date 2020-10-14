4117329f1eefd73d9bc67ae3421063a0b6b0f45c
jwilson5153 <jwilson5153@yahoo.com>
2020-10-14 16:46:57 -0700

    initialize lbs to kilos

---
+++ poundsToKilograms.js
@@ -0,0 +1,21 @@
+/* Defining Table
+ INPUT:  Get number of pounds - x 
+ PROCESSING-1: Convert pounds to kilograms by dividing total pounds by 2.205
+ PROCESSING: Round result to 1 decimal place by 
+ OUTPUT: Display kilograms on the document
+*/ 
+
+
+function poundsToKilograms() {
+	//INPUT get the number of pounds
+	 let x = parseFloat(document.getElementById('number1').value);
+	 let y = 2.205
+	//PROCESSING-1 divide the # of lbs by 2.205
+	 let kiloMass = x / y;
+	//PROCESSING-2 round the result to one digit after the decimal
+	 let digits = 1;
+	 let multiplier = Math.pow(10, digits);
+	 kiloMass = Math.round(kiloMass * multiplier) / multiplier;
+	 //OUTPUT
+	 document.getElementById('output').innerHTML = "The result of " + x + " divided " + y + " equals " + kiloMass;
+}
