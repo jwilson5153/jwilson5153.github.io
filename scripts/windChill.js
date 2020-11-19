3a3b22b2b4ceea0e303259113e1ca5420a53f710
jwilson5153 <wil20141@byui.edu>
2020-11-18 16:04:33 -0800

    20201118Update-9

--- scripts/windChill.js
+++ scripts/windChill.js
@@ -10,22 +10,19 @@ function doInputOutput() {
     //Decide if the specs have been met to calculate windchill. 
     if (t < 50 && s > 3) {
         //Call the windChill function if the specs have been met
-        windChill(t, s)
+        let windchill = windChill(t, s);
+        //OUTPUT
+        //Inform the user about the wind chill
+        document.getElementById("output").innerHTML = "The windchill is " + windchill + " Farenheit";
     } else {
         //Inform the user if the specs have not been met.
         document.getElementById("output").innerHTML = "The temperature must be lower that 50 Farenheit and the windspeed must be more that 3 mph."
     }
-
 }
 //Use the windChill function to calculate the wind chill
 function windChill(t, s) {
     let windchill = (35.74 + 0.6215 * t - 35.75 * Math.pow(s, 0.16) + 0.4275 * t * Math.pow(s, 0.16));
     //Limit windchill to 2 decimal places and return it
-    return windchill = windchill.toFixed(2);
-
-
-
+    windchill = windchill.toFixed(2);
+    return windchill;
 }
-//OUTPUT
-//Inform the user about the wind chill
-document.getElementById("output").innerHTML = "The windchill is " + windchill + " Farenheit";
