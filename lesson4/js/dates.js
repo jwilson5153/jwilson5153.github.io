e42cf42ec3560abf7a6aa2746c6b0741abb69832
jwilson5153 <jwilson5153@yahoo.com>
2021-01-30 13:27:57 -0800

    20210130First

--- lesson4/js/dates.js
+++
@@ -1,6 +0,0 @@
-    let x = document.lastModified;
-    document.getElementById("lastModified").innerHTML = "Last Updated: " + x;
-
-    let d = new Date();
-    let n = d.getFullYear();
-    document.getElementById("thisYear").innerHTML = n;
