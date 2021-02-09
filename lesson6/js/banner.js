e892e735332c19754baaca201ef5de6122f519e1
jwilson5153 <jwilson5153@yahoo.com>
2021-02-08 16:51:25 -0800

    20210208Update2

--- lesson6/js/banner.js
+++ lesson6/js/banner.js
@@ -1,20 +1,9 @@
 // Find day of week to turn banner on and off
 
-var d = new Date();
-var weekday = new Array(7);
-weekday[0] = "Sunday";
-weekday[1] = "Monday";
-weekday[2] = "Tuesday";
-weekday[3] = "Wednesday";
-weekday[4] = "Thursday";
-weekday[5] = "Friday";
-weekday[6] = "Saturday";
+let d = new Date();
 
-var n = weekday[d.getDay()];
-
-if (n === 'Friday') {
-    document.getElementById('banner').className = 'banner';
-} else {
-    document.getElementById('banner').classList.remove('banner');
-    document.getElementById('banner').className = 'bannernone';
+let n = d.getDay();
+let element = document.getElementById('banner');
+if (n === 5) {
+    element.classList.toggle('banner');
 }
