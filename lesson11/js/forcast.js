88e26b45d5a351bdda2f1c2c9ab4ffae5ecabb1b
jwilson5153 <jwilson5153@yahoo.com>
2021-03-17 21:07:06 -0500

    20210317UpdateLastToday

--- lesson11/js/forcast.js
+++ lesson11/js/forcast.js
@@ -1,4 +1,19 @@
-let fcityID = 5604473;
+// Page selector
+if (document.getElementById("preston")){
+    fcityID = 5604473;
+    cityID = 5604473;
+  }
+  else if (document.getElementById("sodasprings")){
+    fcityID = 5678757;
+    cityID = 5678757;
+  }
+  else if (document.getElementById("fishhaven")){
+    fcityID = 5677822;
+    cityID = 5677822;
+  }
+
+  
+
 let fappid = `2d519255bfe7a838f5984fa0bb3aa767`;
 const fapiURL = `https://api.openweathermap.org/data/2.5/forecast?id=${fcityID}&units=imperial&appid=${fappid}`;
 fetch(fapiURL)
@@ -21,3 +36,27 @@ fetch(fapiURL)
             document.getElementById(`imagesrc${day+1}`).setAttribute('alt', image_alt);
         }
     });
+
+    let appid = `2d519255bfe7a838f5984fa0bb3aa767`;
+    const apiURL = `https://api.openweathermap.org/data/2.5/weather?id=${cityID}&units=imperial&appid=${appid}`;
+    fetch(apiURL)
+      .then((response) => response.json())
+      .then((jsObject) => {
+      //  console.log(jsObject);
+        // Weather Summary Temperature and Weather icon JSON load
+        const temperature = document.querySelector('#temperature');
+        temperature.textContent = (jsObject.main.temp).toFixed();
+        const imagesrc = `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+        const desc = jsObject.weather[0].description;
+        document.getElementById('icon').setAttribute('src', imagesrc);
+        document.getElementById('icon').setAttribute('alt', desc);
+        // Weather Summary humidity JSON load
+        const humidity = document.querySelector('#humidity');
+        humidity.textContent = (jsObject.main.humidity).toFixed();
+        // Weather Summary Wind Speed JSON load
+        const windspeed = document.querySelector('#windspeed');
+        windspeed.textContent = (jsObject.wind.speed).toFixed();
+        // Weather Summary WindChill JSON load
+        const windchill = document.querySelector('#windchill');
+        windchill.textContent = (jsObject.main.feels_like).toFixed();
+       });
--- lesson11/js/towns.js
+++ lesson11/js/towns.js
@@ -4,30 +4,30 @@ fetch(requestURL)
         return response.json();
     })
     .then(function (jsonObject) {
-            const towns = jsonObject['towns'];
-            towns.forEach(sitetown => {
-                const card = document.createElement("section");
-                const data = document.createElement("section")
-                const timage = document.createElement("figure")
-                const townname = document.createElement("h2");
-                const townmotto = document.createElement("p");
-                const townyearfounded = document.createElement("p");
-                const towncurrentpopulation = document.createElement("p");
-                const townavgrainfall = document.createElement("p");
-                const image = document.createElement("img");
-                const cards = document.querySelector(".cards");
-                if (`${sitetown.name}` == 'Fish Haven' || `${sitetown.name}` == 'Preston' || `${sitetown.name}` == 'Soda Springs') {
+        const towns = jsonObject['towns'];
+        towns.forEach(sitetown => {
+            const card = document.createElement("section");
+            const data = document.createElement("section")
+            const timage = document.createElement("figure")
+            const townname = document.createElement("h2");
+            const townmotto = document.createElement("p");
+            const townyearfounded = document.createElement("p");
+            const towncurrentpopulation = document.createElement("p");
+            const townavgrainfall = document.createElement("p");
+            const image = document.createElement("img");
+            const cards = document.querySelector(".cards");
+            if (`${sitetown.name}` == 'Fish Haven' || `${sitetown.name}` == 'Preston' || `${sitetown.name}` == 'Soda Springs') {
                 townname.innerHTML = `${sitetown.name}`;
                 townmotto.innerHTML = `${sitetown.motto}`;
-                townyearfounded.innerHTML = "Year Founded: " +  `${sitetown.yearFounded}`;
-                towncurrentpopulation.innerHTML = "Current Population: " +  `${sitetown.currentPopulation}`;
-                townavgrainfall.innerHTML = "Average Rain Fall: " +  `${sitetown.averageRainfall}`;
+                townyearfounded.innerHTML = "Year Founded: " + `${sitetown.yearFounded}`;
+                towncurrentpopulation.innerHTML = "Current Population: " + `${sitetown.currentPopulation}`;
+                townavgrainfall.innerHTML = "Average Rain Fall: " + `${sitetown.averageRainfall}`;
                 image.setAttribute('src', 'images/' + `${sitetown.photo}`);
                 image.setAttribute("alt", `${sitetown.name}` + ' Beauty')
                 timage.append(image);
-                
-                
-                
+
+
+
                 data.append(townname);
                 data.append(townmotto);
                 data.append(townyearfounded);
@@ -37,42 +37,7 @@ fetch(requestURL)
                 card.append(timage)
                 cards.append(card);
 
-                //console.table(jsonObject); // temporary checking for valid response and data parsing
-                /* 
-        const cards = document.querySelector(".cards");
-        const data = document.querySelector(".data"); 
-        const timage = document.querySelector(".timage"); 
-      
-        towns.forEach(sitetown => {
-            let card = document.createElement('section');
-            let h2 = document.createElement('h2');
-            let h3 = document.createElement('h3');
-            let h4 = document.createElement('h4');
-            let h5 = document.createElement('h5');
-            let h6 = document.createElement('h6');
-            let image = document.createElement('img');
-            let alt = document.createElement('alt');
-           if (`${sitetown.name}` == 'Fish Haven' || `${sitetown.name}` == 'Preston' || `${sitetown.name}` == 'Soda Springs') {
-            // const townsfilter = sitetown.filter(x => (sitetown.name == "Fish Haven" || sitetown.name == "Preston" || sitetown.name == "Soda Springs"));
-                h2.innerHTML = `${sitetown.name}`;
-                cards.append2(h2);
-                h3.innerHTML = `${sitetown.motto}`;
-                cards.append(h3);
-                h4.innerHTML = "Year Founded: " + `${sitetown.yearFounded}`;
-                cards.append(h4);
-                h5.innerHTML = "Current Population: " + `${sitetown.currentPopulation}`;
-                cards.append(h5);
-                h6.innerHTML = "Average Rain Fall: " + `${sitetown.averageRainfall}`;
-                cards.append(h6);
-                image.setAttribute('src', 'images/' + `${sitetown.photo}`);
-                cards.append(image);
-                image.setAttribute('alt', `${sitetown.name}` + ' Beauty');
-                cards.append(image);
-              
-                cards.append(card);
-
-            */
-           }
+            }
         })
 
-            });
+    });
